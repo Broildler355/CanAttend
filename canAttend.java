07abd20526b1247df7babd94ceff441abbc28be0
Broildler355 <fouadk2024@gmail.com>
2026-02-12 03:09:57 +0000

    kittens > puppies

--- canAttend.java
+++ canAttend.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
 
 public class CanAttend {
-
-	//precondition: 
-	//postcondition: 
+	//Fouad Kadry, period 4, 2/10/25
+	//precondition: needs an ArrayList of MeetingIntervals to function as the sigular input
+	//postcondition: returns whether or not you can attend all of these meetings based on if their times overlap
 	public static boolean canAttend(ArrayList<MeetingInterval> meetings) {
 		//your implementation here
         System.out.println(meetings);
         for (int i = 0; i < meetings.size(); i++) {
             for(int m = i+1; m< meetings.size(); m++) {
-				if (((meetings.get(i).getStart()-meetings.get(m).getEnd())<0) && ((meetings.get(m).getStart()-meetings.get(i).getEnd())<0))
+				if (((meetings.get(i).getStart()-meetings.get(m).getEnd())<0) && ((meetings.get(m).getStart()-meetings.get(i).getEnd())<0)) {
+
+				}
+				else if (((meetings.get(i).getStart()-meetings.get(m).getEnd())>0) && ((meetings.get(m).getStart()-meetings.get(i).getEnd())>0)) {
+
+				}
+				else {
+					return false;
+				}
             }
 
         }
-		return false; //replace me with actual logic please!
+		return true; //replace me with actual logic please!
 	}
 	public static void main(String[] args) {
 		ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
